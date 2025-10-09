92c71014063e5240756361dcc4fbd5d911f093c5
David Ortega <davidortegacadena@gmail.com>
2025-10-09 02:05:15 -0500

    :sparkles: Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Cta } from './components/cta/cta';
+import { Faq } from './components/faq/faq';
+import { FeaturesAlt } from './components/features-alt/features-alt';
+import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Pricing } from './components/pricing/pricing';
+import { Services } from './components/services/services';
+import { Stats } from './components/stats/stats';
+import { Team } from './components/team/team';
+import { Feature } from './components/feature/feature';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Hero, About, Services, Portfolio, Team, Contact, Stats, Feature, FeaturesAlt, Cta, Faq, Pricing, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('notiflash');
---
+++ src/app/components/features-alt/features-alt.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features-alt',
+  imports: [],
+  templateUrl: './features-alt.html',
+  styleUrl: './features-alt.css'
+})
+export class FeaturesAlt {
+
+}
